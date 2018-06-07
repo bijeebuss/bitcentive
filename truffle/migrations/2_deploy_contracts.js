cead180db65d5cf20577a2b1cfe86e39bb7af70b
Michael Welnick <mwelnick@icloud.com>
2018-06-06 18:37:13 -0700

    build tasks

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var BitCentive = artifacts.require('BitCentive');
 
-module.exports = function(deployer) {
-  deployer.deploy(BitCentive);
+module.exports = function(deployer, network, accounts) {
+  return deployer.deploy(BitCentive)
+  .then(() => BitCentive.deployed())
+  .then(bitcentive => {
+    return bitcentive.setCharity(accounts[1]);
+  });
 };
